38ba7608204478d6101efd135a2dde9c9f6745f0
nmthangdn2000 <uptimumdn2000@gmail.com>
2022-04-11 00:44:54 +0700

    add google drive

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import 'dotenv/config.js';
 import * as server from './start/server';
 import * as database from './start/database';
+import * as drive from './helpers/driver.helper';
 
 (async () => {
   try {
     server.start();
     await database.connect();
-  } catch (error) {
+    await drive.connect();
+    drive.listFiles();
+  } catch (e) {
     console.log(`Cannot start server. Error: ${e}`);
   }
 })();
